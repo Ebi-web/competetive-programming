c6f1a63c38e33d2906e97991ed0f4edf85ba5513
toshiaki <conamn1185@gmail.com>
2022-10-13 22:53:16 +0900

    otoshidama

--- Atcoder/C/Otoshidama.go
+++ Atcoder/C/Otoshidama.go
@@ -1,4 +1,4 @@
-package C
+package main
 
 import (
 	"bufio"
@@ -14,19 +14,22 @@ func main() {
 	input := strings.Split(scanner.Text(), " ")
 	num_bills, _ := strconv.Atoi(input[0])
 	amount, _ := strconv.Atoi(input[1])
-	available_bills := [3]int{10000, 5000, 1000}
 	bills := map[int]int{
-		10000: 0,
-		5000:  0,
-		1000:  0,
+		10000: -1,
+		5000:  -1,
+		1000:  -1,
 	}
-	for available_bill := range available_bills {
-		if amount < available_bill {
-			continue
+	for i := 0; i <= num_bills; i++ {
+		for j := 0; j <= num_bills-i; j++ {
+			k := num_bills - i - j
+			if 10000*i+5000*j+1000*k == amount {
+				bills[10000] = i
+				bills[5000] = j
+				bills[1000] = k
+				break
+			}
 		}
-
 	}
-	for bill := range bills {
-		fmt.Print(bill)
-	}
+	//	stdout bills with space
+	fmt.Print(bills[10000], " ", bills[5000], " ", bills[1000])
 }
